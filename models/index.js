8754ac41b1d3d4494c0e5cff91e00d5a64fa39e4
username <xinjiewang@XinjiedeMacBook-Pro.local>
2024-02-13 14:19:24 -0500

    2nd commit

--- models/index.js
+++ models/index.js
@@ -1,19 +1,25 @@
 const mongoose = require('mongoose')
-const CuisineSchema = require('./cuisine')
-const RecipeSchema = require('./recipe')
-const DirectionSchema = require('./direction')
-const IngredientSchema = require('./ingredient')
+const BrandSchema = require('./brand')
+const CartItemSchema = require('./cartItem')
+const CategorySchema = require('./category')
+const ProductSchema = require('./ingredient')
+const ShoppingCartSchema = require('./shoppingCart')
+const UserSchema = require('./user')
 
 //we can give them any name we want, but like so much else in JS, it would be counterintuitive to not give it a semantic, recognizable name
-const Cuisine  = mongoose.model('Cuisine', CuisineSchema )
-const Recipe = mongoose.model('Recipe', RecipeSchema)
-const Direction = mongoose.model('Direction', DirectionSchema)
-const Ingredient = mongoose.model('Ingredient', IngredientSchema)
+const Brand  = mongoose.model('Brand', BrandSchema )
+const CartItem = mongoose.model('CartItem', CartItemSchema)
+const Category = mongoose.model('Category', CategorySchema)
+const Product = mongoose.model('Product', ProductSchema)
+const ShoppingCart = mongoose.model('ShoppingCart', ShoppingCartSchema)
+const User = mongoose.model('User', UserSchema)
 
 
 module.exports = {
-    Cuisine,
-    Recipe,
-    Direction,
-    Ingredient 
+    Brand,
+    CartItem,
+    Category,
+    Product, 
+    ShoppingCart,
+    User
 }
---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const  { Schema } = require('mongoose')
+
+const User = new Schema(
+    {
+        user_name: { type: String, required: true },
+        email: { type: String, required: true },
+        address: { type: String, required: true },
+        phone: { type: String, required: true },
+        password: { type: String, required: true }
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('users', User)
